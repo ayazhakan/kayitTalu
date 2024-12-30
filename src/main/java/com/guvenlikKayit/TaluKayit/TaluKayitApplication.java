ba5cea1c461a5963055db6812fdf534dea7f995c
unknown <hakanayaz9844@outlook.com>
2024-12-30 01:25:07 +0100

    tomcat buildi icin gerekli duzenlemeler yapildi

--- src/main/java/com/guvenlikKayit/TaluKayit/TaluKayitApplication.java
+++ src/main/java/com/guvenlikKayit/TaluKayit/TaluKayitApplication.java
@@ -19,25 +19,21 @@ public class TaluKayitApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TaluKayitApplication.class, args);
 	}
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TaluKayitApplication.class);
 	}
-
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new InMemoryUserDetailsManager(
 				User.withUsername("guvenlik")
-						.password("{noop}talu")  // {noop} şifresiz çözümleme
+						.password("{noop}talu")
 						.roles("ADMIN")
 						.build()
 		);
 	}
-
 	@Configuration
 	public static class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
-
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			super.configure(http);
--- src/main/java/com/guvenlikKayit/TaluKayit/ui/LoginView.java
+++ src/main/java/com/guvenlikKayit/TaluKayit/ui/LoginView.java
@@ -1,27 +1,33 @@
 package com.guvenlikKayit.TaluKayit.ui;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet("/login")
-@Route("/login")  // /login yoluna gelen istekler burada karşılanır
+@Route("/login")
+@AnonymousAllowed
 public class LoginView extends Composite<LoginOverlay> {
 
     public LoginView() {
-        // LoginOverlay sayfasının başlık ve özelliklerini ayarlıyoruz
         getContent().setTitle("Talu Tekstil Arac Takip Kayit");
-        getContent().setOpened(true);  // Overlay'i açıyoruz
-        getContent().setError(false);  // Hata durumunu false yapıyoruz
-        getContent().setAction("login");  // Formun 'action' kısmını 'login' olarak belirliyoruz
+        getContent().setOpened(true);
+        getContent().setError(false);
+        getContent().setAction("login");
 
-        // I18n (Uluslararasılaştırma) ayarları
         LoginI18n i18n = LoginI18n.createDefault();
         i18n.setAdditionalInformation("Contact Hakan AYAZ if you're experiencing issues logging into your account");
         getContent().setI18n(i18n);
-        getContent().setForgotPasswordButtonVisible(false);  // Şifre unuttum butonunu gizliyoruz
+        getContent().setForgotPasswordButtonVisible(false);
+
+        getContent().addLoginListener(e -> {
+            if (e.getPassword().equals("talu") && e.getUsername().equals("guvenlik")) {
+                UI.getCurrent().navigate("admin");
+            } else {
+                getContent().setError(true);
+            }
+        });
     }
 }
